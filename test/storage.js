4993b790d2fc651360ef1b98e3a668b634eb20ce
trkm <trkm2@inbox.ru>
2018-07-16 12:53:43 +0300

    Add files via upload

---
+++ test/storage.js
@@ -0,0 +1,15 @@
+function initToken(token){
+    return true
+}
+
+function listBooks(){
+    return ['a', 'b']    
+}
+
+function loadBook(name, psw){
+    return ['x-' + name, 'y-' + name]
+}
+
+function saveBook(name, obj, psw, cb){
+    return true
+}
