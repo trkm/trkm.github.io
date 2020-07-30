8caef0f3f3f42c0eebf0e5126adf411d1c0d3ef4
trkm <trkm2@inbox.ru>
2020-07-30 10:14:56 +0300

    Update p.js

--- p.js
+++ p.js
@@ -1,5 +1,5 @@
 function FindProxyForURL(url, host) {
-    if(shExpMatch(host, "(*ank.ru*)") || shExpMatch(host, "(*eppel*)") || shExpMatch(host, "(*linked*)")){
+    if(shExpMatch(host, "(*ank.ru*)") || shExpMatch(host, "(*eppel*)") || shExpMatch(host, "(*linked*)") || shExpMatch(host, "(*10.*)")){
         return "SOCKS5 127.0.0.1:3128";
     }
     return "DIRECT";
