3fc8ba2634442bdafc345c88abdfb50ad3e3cf5b
trkm <trkm2@inbox.ru>
2020-07-30 10:34:14 +0300

    Update p.js

--- p.js
+++ p.js
@@ -1,5 +1,10 @@
 function FindProxyForURL(url, host) {
-    if(shExpMatch(host, "(*ank.ru*)") || shExpMatch(host, "(*eppel*)") || shExpMatch(host, "(*linked*)") || shExpMatch(host, "(*10.*)")){
+    /*
+    if(shExpMatch(host, "(*ank.ru*)") || shExpMatch(host, "(*eppel*)") || shExpMatch(host, "(*linked*)")){
+        return "SOCKS5 127.0.0.1:3128";
+    }
+    */
+    if(isInNet(host, "10.0.0.0", "255.0.0.0")){
         return "SOCKS5 127.0.0.1:3128";
     }
     return "DIRECT";
